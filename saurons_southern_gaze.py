545be90a45900b8f045688965f77406a92bcc901
bwbellmath <bwbell@math.arizona.edu>
2021-04-12 11:05:17 -0700

    updated README and cleaned up print statements

--- saurons_southern_gaze.py
+++ saurons_southern_gaze.py
@@ -26,15 +26,18 @@ H = np.sqrt(R**2 - (cxof/2)**2)
 lwidth = 0.1
 
 annotate_index = sys.argv[1]
-if (sys.argv[1] == "-i"):
+if (len(sys.argv) < 2):
+  print("Missing \"Row Progress\" argument (e.g. 10)")
+  annotate_index = 0
+elif (sys.argv[1] == "-i"):
+  # if you're running this manually
   annotate_index = 0
 else:
-  annotate_index = sys.argv[1]
+  annotate_index = int(sys.argv[1])+1
   
-print("rows in: {}".format(annotate_index))
-print("rows in: {}".format(annotate_index), file=open("southern_progress.txt", "w"))
+print("Progress: {} rows".format(annotate_index))
+print("Progress: {} rows".format(annotate_index).format(annotate_index), file=open("southern_progress.txt", "w"))
 with open(fo, "w") as text_file:
-
   print(str_begin, file=text_file)
 
   str_next = "<path\n     style=\"fill:none;stroke:#ffffff;stroke-width:{}px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\"\n     d=\"m {},{} {},{} z\"\n     id=\"path{}\"\n     inkscape:connector-curvature=\"0\" />".format(lwidth*4, cx, cy-H, 0, 2*H, 124412)
@@ -44,19 +47,21 @@ with open(fo, "w") as text_file:
   # add stitch lines to eye segments
   
   jjj = 100001
-  print("pre : {}".format(pre))
+  print("Progress drawn in RED")
+  print("Drawing {} rows of pupil".format(len(pre)))
+  #print("pre : {}".format(pre))
   for r in pre[1:]:
     j = int(np.floor(r/sr)+1)
     jj = int(np.floor(r/(sr/2) + 1))
     if (jjj - 100000 < int(annotate_index)):
-      print("Printing Red Ring {}".format(j))
+      #print("Printing Red Ring {}".format(j))
       colore="#ff0000"
     else:
       colore="#ffffff"
 
-    print("j: {}, r/sr: {}".format(j, r/sr))
+    #print("j: {}, r/sr: {}".format(j, r/sr))
     if True: #np.abs(j - r/sr) > 0.7:
-      print("j: {} should get a line!".format(j, r/sr))
+      #print("j: {} should get a line!".format(j, r/sr))
       dangle = (2*np.pi)/(j*10+1)
       angles = np.arange(0, np.arccos(L/r), dangle)
       #angles = np.linspace(0, 
@@ -123,7 +128,7 @@ with open(fo, "w") as text_file:
         k+=1
 
         
-    print("generating eye segments of radius r: {}".format(r))
+    #print("generating eye segments of radius r: {}".format(r))
     mod_factor = 1
     if (jjj % 2 == 0):
       mod_factor = 2.0
@@ -139,7 +144,7 @@ with open(fo, "w") as text_file:
     ra_b = offset
     ra_e = 2*np.pi-offset
     # right side
-    print("generating eye segments of radius r: {}".format(r))
+    #print("generating eye segments of radius r: {}".format(r))
     str_before_l =" <path\n      sodipodi:cx=\"311.26001\"\n sodipodi:cy=\"715.59003\"\n sodipodi:rx=\"{}\"\n sodipodi:ry=\"{}\"\n sodipodi:end=\"{}\"\n sodipodi:start=\"{}\"\n sodipodi:open=\"true\"\n sodipodi:type=\"arc\"\n d=\"m 1350.0359,296.84816 a {},{} 0 0 1 -7.1876,854.89044\"\n id=\"path31910\"\n style=\"fill:none;fill-opacity:1;stroke:{};stroke-width:{};stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1\" />".format(r, r, ra_b, ra_e, r, r, colore, mod_factor*0.1)
 
     str_before_r = "<path \n style=\"fill:none;fill-opacity:1;stroke:{};stroke-width:{};stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1\"\n id=\"path31893\"\n d=\"M 1376.0413,1143.0503 A {},{} 0 0 1 1362.4285,322.71281\"\n sodipodi:type=\"arc\"\n sodipodi:open=\"true\"\n sodipodi:start=\"{}\"\n sodipodi:end=\"{}\"\n sodipodi:ry=\"{}\"\n sodipodi:rx=\"{}\"\n sodipodi:cy=\"715.59003\"\n sodipodi:cx=\"2411.26\" /> ".format(colore, mod_factor*0.1, r, r, la_e, la_b, r, r)
@@ -162,7 +167,7 @@ with open(fo, "w") as text_file:
       mod_factor = 8.0
 
     if (j < int(annotate_index)):
-      print("Printing Red Ring {}".format(j))
+      #print("Printing Red Ring {}".format(j))
       colore="#ff0000"
     else:
       colore="#ffffff"
@@ -192,13 +197,14 @@ with open(fo, "w") as text_file:
   i = 999
   # j is counting stitches per round divided by 10
   j = 1
+  print("Drawing {} rows of iris/sclera".format(len(pra)))
   for r in pra[:-1]:
     #print(r)
-    print("j : {}, r/sr: {}".format(j, r/sr))
+    #print("j : {}, r/sr: {}".format(j, r/sr))
     angles = np.linspace(0, 2*np.pi, j*10+1)
     k = 0
     if (j < int(annotate_index)/2+1):
-      print("Printing Red Ring {}".format(j))
+      #print("Printing Red Ring {}".format(j))
       colore="#ff0000"
     else:
       colore="#ffffff"
@@ -231,7 +237,7 @@ with open(fo, "w") as text_file:
       if (dl > R) | (dr > R):
         #if starting points are interior, then shorten to intersection
         if (dli < R) & (dri < R):
-          print("we're interior! {} < {} & {} < {}".format(dli, R, dri, R))
+          #print("we're interior! {} < {} & {} < {}".format(dli, R, dri, R))
           # are we on the left or right?
           if (a >= 0) & (a < np.pi/2):
             # angle of reference is pi minus angle?
@@ -246,10 +252,10 @@ with open(fo, "w") as text_file:
 
           theta_l = np.arcsin(L/R*np.sin(aa))
           theta_r = np.pi-aa-theta_l
-          print("angles: theta_a: {}, theta_l: {}, theta_r: {}".format(aa, theta_l, theta_r))
+          #print("angles: theta_a: {}, theta_l: {}, theta_r: {}".format(aa, theta_l, theta_r))
           rp = np.sqrt(R**2 + L**2 - 2*L*R*np.cos(theta_r))
           dr = r - rp
-          print("sr = {}, dr: {} = {} - {} = r - rp".format(sr, dr, r, rp))
+          #print("sr = {}, dr: {} = {} - {} = r - rp".format(sr, dr, r, rp))
           xo = xo -dr*np.cos(a)#+ (sr-dr)*np.cos(a)
           yo = yo -dr*np.sin(a)#+ (sr-dr)*np.sin(a)
           xp = (dr+sr)*np.cos(a)
